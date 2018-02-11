e88e3c39131dc84e4e9317043a7412928f715724
Siddhant Bhardwaj <siddhant.bhardwaj16@gmail.com>
2018-02-11 15:09:31 -0400

    fixed styles on events list page

--- goSomewhereProj/src/screens/map_view_screen.js
+++ goSomewhereProj/src/screens/map_view_screen.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import Mapbox from '@mapbox/react-native-mapbox-gl';
 
-const events = [{"id":1,"title":"Hackathon","description":null,"start_at":"2018-02-10T14:10:52.773Z","end_at":"2018-02-11T20:12:37.044Z","attendees":null,"created_at":"2018-02-10T18:12:44.050Z","updated_at":"2018-02-10T18:12:44.050Z","latitude":44.6374247,"longitude":-63.5872094,"address":"Goldberg Computer Science Building, 6050 University Ave, Halifax, NS B3H 1W5"},{"id":2,"title":"Party after winning Hackathon","description":"Please bring your own drink","start_at":"2018-02-11T22:19:45.595Z","end_at":null,"attendees":null,"created_at":"2018-02-10T18:21:52.274Z","updated_at":"2018-02-10T18:21:52.274Z","latitude":44.6386448,"longitude":-63.5919118,"address":"H-1422B, 6230 Coburg Road, Halifax, NS, B3h4R2"}];
+const events = [{"id":1,"title":"Android Hackathon","description":null,"start_at":"2018-02-10T14:10:52.773Z","end_at":"2018-02-11T20:12:37.044Z","attendees":null,"created_at":"2018-02-10T18:12:44.050Z","updated_at":"2018-02-10T18:12:44.050Z","latitude":44.6374247,"longitude":-63.5872094,"address":"Goldberg Computer Science Building, 6050 University Ave, Halifax, NS B3H 1W5"},{"id":2,"title":"Party after winning Hackathon","description":"Please bring your own drink","start_at":"2018-02-11T22:19:45.595Z","end_at":null,"attendees":null,"created_at":"2018-02-10T18:21:52.274Z","updated_at":"2018-02-10T18:21:52.274Z","latitude":44.6386448,"longitude":-63.5919118,"address":"H-1422B, 6230 Coburg Road, Halifax, NS, B3h4R2"}];
 
 // Code referred from mapbox documentation https://www.mapbox.com/help/first-steps-react-native-sdk/
 Mapbox.setAccessToken('pk.eyJ1IjoiYXltYW54ZCIsImEiOiJjamRobDYwdnkwYWF4MnFwcjM5ZmZxbGJrIn0.xa6xkZ0yUe3An3stHXd4rg');
@@ -21,7 +21,8 @@ export default class Map_View_Screen extends React.Component {
       <Mapbox.PointAnnotation
         key={`pi${event.id}`}
         id={`pi${event.id}`}
-        coordinate={[event.longitude, event.latitude]}>
+        coordinate={[event.longitude, event.latitude]}
+        onSelected={this.toEventDetails}>
         <View style={styles.annotationContainer}>
           <View style={styles.annotationFill} />
         </View>
@@ -41,6 +42,9 @@ export default class Map_View_Screen extends React.Component {
         {this.renderAnnotations(events)}
       </Mapbox.MapView>
     )
+  }
+  toEventDetails = () => {
+    this.props.navigation.navigate('Event');
   }
 }
 
