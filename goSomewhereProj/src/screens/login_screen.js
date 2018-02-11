672d9b3d5796fb9e6a1c91f268178dc90761cc03
Matthew Hemming <mbhemming@icloud.com>
2018-02-11 12:16:58 -0400

    Merge branch 'init'

--- goSomewhereProj/src/screens/login_screen.js
+++ goSomewhereProj/src/screens/login_screen.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    Navigator,
-    TextInput,
-    KeyboardAvoidingView,
-    TouchableOpacity,
-    Dimensions,
-    AsyncStorage,
-    ScrollView,
- } from 'react-native';
+   StyleSheet,
+   Text,
+   View,
+   Navigator,
+   TextInput,
+   KeyboardAvoidingView,
+   TouchableOpacity,
+   Dimensions,
+   AsyncStorage,
+   ScrollView
+} from 'react-native';
 
- import globalStyle  from '../styles/Global_Container_Style'
+import globalStyle from '../styles/Global_Container_Style'
 
-
-
-import {
-  StackNavigator,
-} from 'react-navigation';
+import {StackNavigator} from 'react-navigation';
 
 const dimensions = Dimensions.get('window');
 //const imageHeight = Math.round(dimensions.width * 16 / 9);
@@ -26,219 +22,178 @@ const getWidth = dimensions.width;
 
 export default class Login_Screen extends React.Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            username: '',
-            password: '',
-            list: [],  //starting with empty array so its allocated before the fetch method works
-        }
-    }
+   constructor(props) {
+      super(props);
+      this.state = {
+         username: '',
+         password: '',
+         list: [], //starting with empty array so its allocated before the fetch method works
+      }
+   }
 
+   //check to see if user has logged in already
+   componentDidMount() {
+      this._loadInitialState().done();
+   }
 
-    //check to see if user has logged in already
-    componentDidMount(){
-        this._loadInitialState().done();
-    }
+   //get info from async storage
+   _loadInitialState = async () => {
+      var value = await AsyncStorage.getItem('user');
 
-    //get info from async storage
-    _loadInitialState = async () => {
-        var value = await  AsyncStorage.getItem('user');
+      if (value != null) { //if the user is already logged in
+         this.props.navigation.navigate('Profile'); //**profile page that we will create later
+      }
+   }
 
-        if(value != null){   //if the user is already logged in
-            this.props.navigation.navigate('Profile');      //**profile page that we will create later
-        }
-    }
+   componentWillMount() {}
 
-    componentWillMount(){
+   render() {
 
-    }
+      return (<KeyboardAvoidingView behavior='padding' style={globalStyle.globalContainerStyle}>
 
-    render() {
+         <ScrollView >
+             <View style={globalStyle.globalContainerStyle}>
 
-      return (
-        //<View>
+            <View>
+               <Text style={styles.header}>
+                  --- GoSomewhere ---
+               </Text>
+               <Text style={styles.header}>
+                  login
+               </Text>
 
-          <KeyboardAvoidingView behavior = 'padding' style = {globalStyle.globalContainerStyle}>
+               <View style={globalStyle.textInputContainer}>
+                  <TextInput style={{
+                        height: 50,
+                        backgroundColor: 'white'
+                     }} placeholder='Username' onChangeText={(username) => this.setState({username})}/>
 
-              <ScrollView>
-              <View style = {globalStyle.globalContainerStyle}>
-                  <Text style={styles.header}> --- GoSomewhere --- </Text>
-                  <Text style={styles.header}> login </Text>
+                  <TextInput style={{
+                        height: 50,
+                        backgroundColor: 'white'
+                     }} secureTextEntry={true} placeholder='Password' onChangeText={(password) => this.setState({password})}/>
+               </View>
 
-                  <View style={globalStyle.textInputContainer}>
-                  <TextInput
-                      style={{height: 50,backgroundColor: 'white',}}
-                       placeholder='Username'
-                      onChangeText={(username) => this.setState({username})}
-                  />
+               <TouchableOpacity style={styles.btn} onPress={this.login}>
+                  <Text>Log in</Text>
+               </TouchableOpacity>
 
+               <TouchableOpacity style={styles.clearBtn} onPress={this.login}>
+                  <Text>forgot password?</Text>
+               </TouchableOpacity>
 
-                  <TextInput
-                      style={{height: 50,backgroundColor: 'white',}}
-                      secureTextEntry={true}
-                       placeholder='Password'
-                      onChangeText={(password) => this.setState({password})}
-                  />
-                  </View>
+            </View>
 
+            
 
-                  <TouchableOpacity
-                      style={styles.btn}
-                      onPress = {this.login}>
-                      <Text>Log in</Text>
+            <View>
+
+               <TouchableOpacity style={styles.signupBtn} onPress={this.toSignUp}>
+                  <Text>Create Account</Text>
+               </TouchableOpacity>
+
+               <View style={styles.shortcutBtn}>
+                  <TouchableOpacity style={styles.btn} onPress={this.toMapView}>
+                     <Text>shorcut to mapview</Text>
                   </TouchableOpacity>
 
-                  <TouchableOpacity
-                      style={styles.clearBtn}
-                      onPress = {this.login}>
-                      <Text>forgot password?</Text>
+                  <TouchableOpacity style={styles.btn} onPress={this.toListView}>
+                     <Text>shorcut to list view</Text>
                   </TouchableOpacity>
 
-                  <TouchableOpacity
-                      style={styles.signupBtn}
-                      onPress = {this.toSignUp}>
-                      <Text>Create Account</Text>
+                  <TouchableOpacity style={styles.btn} onPress={this.toEventDetails}>
+                     <Text>to event details</Text>
                   </TouchableOpacity>
+               </View>
 
-                  <View style={styles.shortcutBtn}>
-                      <TouchableOpacity
-                          style={styles.btn}
-                          onPress = {this.toMapView}>
-                          <Text>shorcut to mapview</Text>
-                      </TouchableOpacity>
+            </View>
 
-                      <TouchableOpacity
-                          style={styles.btn}
-                          onPress = {this.toListView}>
-                          <Text>shorcut to list view</Text>
-                      </TouchableOpacity>
+        </View>
+         </ScrollView>
+      </KeyboardAvoidingView>);
+   }
 
-                      <TouchableOpacity
-                          style={styles.btn}
-                          onPress = {this.toEventDetails}>
-                          <Text>to event details</Text>
-                      </TouchableOpacity>
-                  </View>
+   toMapView = () => {
+      this.props.navigation.navigate('Map');
+   }
 
+   toListView = () => {
+      this.props.navigation.navigate('ListView');
+   }
 
+   toEventDetails = () => {
+      this.props.navigation.navigate('Event');
+   }
 
-              </View>
+   toSignUp = () => {
+      this.props.navigation.navigate('SignUp');
+   }
 
+   login = () => {
 
-            </ScrollView>
-          </KeyboardAvoidingView>
+      //send to server
+      fetch('http://127.0.0.1:3000/v1/users.json', {
+         method: 'POST',
+         headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+         },
+         body: JSON.stringify({username: this.state.username, password: this.state.password})
+      })
 
+      //handle response
+         .then((response) => response.json()).then((res) => {
+         //alert('hello');
+         console.log('response happened1');
+         console.log(res);
 
+         //if user and pass exists, then log them in
+         if (res.success === true) {
+            console.log('user exists in DB');
+            //AysncStorage.setItem('user',res.user);   may need this later
+            this.props.navigation.navigate('Profile') //else, tell the user they dont exist in the database; navigate user to profile page
+         } else {
+            console.log('user was not in the database' + res.message);
+         }
+      }).catch((err) => {
+         console.log(err);
+      }).done();
 
-        //</View>
-      );
-    }
+      //
 
-    toMapView = () => {
-        this.props.navigation.navigate('Map');
-    }
+   }
 
-    toListView = () => {
-        this.props.navigation.navigate('ListView');
-    }
-
-    toEventDetails = () => {
-        this.props.navigation.navigate('Event');
-    }
-
-    toSignUp = () => {
-        this.props.navigation.navigate('SignUp');
-    }
-
-
-    login = () => {
-
-
-        //send to server
-        fetch('http://127.0.0.1:3000/v1/users.json', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                username: this.state.username,
-                password: this.state.password,
-
-            })
-        })
-
-        //handle response
-        .then((response) => response.json())
-        .then((res) => {
-            //alert('hello');
-            console.log('response happened1');
-            console.log(res);
-
-              //if user and pass exists, then log them in
-              if(res.success === true){
-                  console.log('user exists in DB');
-                  //AysncStorage.setItem('user',res.user);   may need this later
-                  this.props.navigation.navigate('Profile'); //navigate user to profile page
-              }
-              //else, tell the user they dont exist in the database
-              else{
-                  console.log('user was not in the database' + res.message);
-              }
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-        .done();
-
-
-        //
-
-    }
-
-    componentWillMount(){
+   componentWillMount() {}
 
 }
-
-
-
-
-
-
-
-
-
-
-}
-
 
 const styles = StyleSheet.create({
 
-    wrapper: {
+   wrapper: {
+      flex: 1,
+      backgroundColor: 'green'
+   },
+
+   container: {
       flex: 1,
       backgroundColor: 'green',
-    },
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingLeft: 40,
+      paddingRight: 40
+   },
 
-  container: {
-    flex: 1,
-    backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingLeft: 40,
-    paddingRight: 40,
-  },
+   header: {
+      fontSize: 24,
+      marginBottom: 60,
+      color: '#fff',
+      justifyContent: 'center',
+      alignSelf: 'center',
+      paddingLeft: 40,
+      paddingRight: 40
+   },
 
-  header: {
-    fontSize: 24,
-    marginBottom: 60,
-    color: '#fff',
-    justifyContent: 'center',
-    paddingLeft: 40,
-    paddingRight: 40,
-  },
-
-  /*
+   /*
   textInput: {
     alignSelf: 'stretch',
     paddingLeft: 16,
@@ -248,42 +203,41 @@ const styles = StyleSheet.create({
 
   },
   */
-  btn: {
+   btn: {
       alignSelf: 'center',
       padding: 20,
       marginBottom: 20,
       backgroundColor: '#01c853',
       alignItems: 'center',
       justifyContent: 'center',
-      width: getWidth/2.5,
-
-  },
-  clearBtn:{
+      width: getWidth / 2.5
+   },
+   clearBtn: {
       alignSelf: 'center',
       padding: 20,
       marginBottom: 20,
       backgroundColor: 'green',
       alignItems: 'center',
       justifyContent: 'center',
-      width: getWidth/2.5,
-  },
-  signupBtn:{
+      width: getWidth / 2.5
+   },
+   signupBtn: {
       alignSelf: 'center',
       padding: 20,
       marginBottom: 20,
       backgroundColor: 'red',
       alignItems: 'center',
       justifyContent: 'center',
-      width: getWidth/1.1,
-  },
+      width: getWidth / 1.1
+   },
 
-  shortcutBtn:{
+   shortcutBtn: {
       alignSelf: 'center',
       padding: 20,
       marginBottom: 20,
       backgroundColor: 'blue',
       alignItems: 'center',
       justifyContent: 'center',
-      width: getWidth/1.1,
-  },
+      width: getWidth / 1.1
+   }
 });
