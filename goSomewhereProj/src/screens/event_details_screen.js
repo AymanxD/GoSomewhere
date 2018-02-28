0ef8b55451b192bb66c051923bee8c00fc764314
Andrey Almabekov <almabekov@gmail.com>
2018-02-28 12:47:10 -0400

    Event image depends on the event category now
    Now each category would have separate image in the event details page

--- goSomewhereProj/src/screens/event_details_screen.js
+++ goSomewhereProj/src/screens/event_details_screen.js
@@ -4,14 +4,27 @@ import {View, Image, ScrollView, Alert, Dimensions, Linking, Share, TouchableOpa
 import {Button} from 'react-native';
 import globalContainerStyle  from '../styles/Global_Container_Style'
 
-const events = [{"id":1,"title":"Android Hackathon","description":null,"start_at":"2018-02-23T14:10:52.773Z","end_at":"2018-02-23T20:12:37.044Z","attendees":null,"created_at":"2018-02-10T18:12:44.050Z","updated_at":"2018-02-10T18:12:44.050Z","latitude":44.6374257,"longitude":-63.5872094,"address":"Goldberg Computer Science Building, 6050 University Ave, Halifax, NS B3H 1W5"},{"id":2,"title":"Party after winning Hackathon","description":"Please bring your own drink","start_at":"2018-02-11T22:19:45.595Z","end_at":null,"attendees":null,"created_at":"2018-02-10T18:21:52.274Z","updated_at":"2018-02-10T18:21:52.274Z","latitude":44.6386448,"longitude":-63.5919118,"address":"H-1422B, 6230 Coburg Road, Halifax, NS, B3h4R2"}];
+const events = [{"id":1,"title":"Android Hackathon","category":"study","description":null,"start_at":"2018-02-23T14:10:52.773Z","end_at":"2018-02-23T20:12:37.044Z","attendees":null,"created_at":"2018-02-10T18:12:44.050Z","updated_at":"2018-02-10T18:12:44.050Z","latitude":44.6374257,"longitude":-63.5872094,"address":"Goldberg Computer Science Building, 6050 University Ave, Halifax, NS B3H 1W5"},{"id":2,"title":"Party after winning Hackathon","category":"party","description":"Please bring your own drink","start_at":"2018-02-11T22:19:45.595Z","end_at":null,"attendees":null,"created_at":"2018-02-10T18:21:52.274Z","updated_at":"2018-02-10T18:21:52.274Z","latitude":44.6386448,"longitude":-63.5919118,"address":"H-1422B, 6230 Coburg Road, Halifax, NS, B3h4R2"}];
+
+const image_categories = [{"party":"party_category_image.jpg","study":"Computer-Cat.jpg"}];
+
 pressedLike='black';
 pressedGoing='black';
+
+//https://stackoverflow.com/questions/37841236/render-images-sources-from-parsed-array-of-objects-in-react-native
+images = [{"party":require("../components/event_details_comps/party_category_image.jpeg"),"study" : require("../components/event_details_comps/Computer-Cat.jpg")}];
+
+
+
+
 export default class Event_Details_Screen extends React.Component {
 constructor(props) {
   super(props);
   this.state = {colorLike:'black', colorGoing:'black'};
   id=this.props.navigation.state.params.id;
+  category=events[id]['category'];
+
+
 }
 
 
@@ -26,7 +39,7 @@ constructor(props) {
        justifyContent: 'space-around',
      }}>
     <ScrollView>
-    <Image source={require('../components/event_details_comps/Computer-Cat.jpg')}
+    <Image source={images[0][category]}
       style={{flex:1, height:200, width: 380, marginBottom: 20}} />
 
      <View
@@ -69,7 +82,7 @@ constructor(props) {
      <View style={{ padding: 10 }}>
        <Text>Event name: {events[id]['title']}</Text>
        <Text>Date: {events[id]['start_at']}</Text>
-       <Text>Category: {events[id]['category']}</Text>
+       <Text>Category: {category}</Text>
        <Text>Address: {events[id]['address']}</Text>
        <Text></Text>
        <Text>{events[id]['description']}</Text>
