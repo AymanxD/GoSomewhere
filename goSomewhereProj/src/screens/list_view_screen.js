10adb17a801b5e32478be0b55e0ebdabcf73d96c
Ayman Mohatarem <aymanmohatarem@gmail.com>
2018-03-02 12:05:12 -0400

    Added API

--- goSomewhereProj/src/screens/list_view_screen.js
+++ goSomewhereProj/src/screens/list_view_screen.js
@@ -14,6 +14,34 @@ import {
   StackNavigator,
 } from 'react-navigation';
 
+
+                // {
+                // "id" : 1,
+                // "title" : 'Hackathon',
+                // "description" : null,
+                // "start_at" : '2018-02-10T14:10:52.773Z',
+                // "end_at" : '2018-02-11T20:12:37.044Z',
+                // "attendees" : null,
+                // "created_at" : '2018-02-10T18:12:44.050Z',
+                // "updated_at" : '2018-02-10T18:12:44.050Z',
+                // "latitude" : 44.6374247,
+                // "longitude" : -63.5872094,
+                // "address" : 'Goldberg Computer Science Building, 6050 University Ave, Halifax, NS B3H 1W5'
+                // },
+                // {
+                // "id" : 2,
+                // "title" : 'Party after winning Hackathon',
+                // "description" : 'Please bring your own drink',
+                // "start_at" : '2018-02-11T22:19:45.595Z',
+                // "end_at" : null,
+                // "attendees" : null,
+                // "created_at" : '2018-02-10T18:21:52.274Z',
+                // "updated_at" : '2018-02-10T18:21:52.274Z',
+                // "latitude" : 44.6386448,
+                // "longitude" : -63.5919118,
+                // "address" : 'H-1422B, 6230 Coburg Road, Halifax, NS, B3h4R2'
+                // }
+
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
 export default class List_View_Screen extends React.Component {
@@ -22,38 +50,29 @@ export default class List_View_Screen extends React.Component {
         super(props);
         this.state = {
             latitude: null,
+            events: [],
             longitude: null,
             error: null,
-            dataSource: ds.cloneWithRows([
-              {
-              "id" : 1,
-              "title" : 'Hackathon',
-              "description" : null,
-              "start_at" : '2018-02-10T14:10:52.773Z',
-              "end_at" : '2018-02-11T20:12:37.044Z',
-              "attendees" : null,
-              "created_at" : '2018-02-10T18:12:44.050Z',
-              "updated_at" : '2018-02-10T18:12:44.050Z',
-              "latitude" : 44.6374247,
-              "longitude" : -63.5872094,
-              "address" : 'Goldberg Computer Science Building, 6050 University Ave, Halifax, NS B3H 1W5'
-              },
-              {
-              "id" : 2,
-              "title" : 'Party after winning Hackathon',
-              "description" : 'Please bring your own drink',
-              "start_at" : '2018-02-11T22:19:45.595Z',
-              "end_at" : null,
-              "attendees" : null,
-              "created_at" : '2018-02-10T18:21:52.274Z',
-              "updated_at" : '2018-02-10T18:21:52.274Z',
-              "latitude" : 44.6386448,
-              "longitude" : -63.5919118,
-              "address" : 'H-1422B, 6230 Coburg Road, Halifax, NS, B3h4R2'
-              }
-          ]),
           interested: 'star-o'
         }
+    }
+
+    componentWillMount(){
+       Alert.alert("events");
+
+      fetch('https://gosomewhere-backend.herokuapp.com/events', {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        }
+      })
+        .then((response) => response.json())
+        .then((responseJson) => {
+          this.setState({events: responseJson});
+        });
+
+        navigator.geolocation.clearWatch(this.watchId);
     }
 
   componentDidMount() {
@@ -71,19 +90,12 @@ export default class List_View_Screen extends React.Component {
     );
   }
 
-  componentWillMount() {
-  //this.fetchData();
-
-  //location services
-  navigator.geolocation.clearWatch(this.watchId);
-}
 
   _renderRow(rowData) {
     return(
       <View>
         <View style={styles.row}>
          <Text style={styles.title}>{rowData.title}</Text>
-         <Text style={styles.description}>{rowData.description}</Text>
          <View style={styles.interest}>
           <Text style={styles.description}>Are you interested?</Text>
           <TouchableHighlight onPress={this._interested.bind(this)}>
@@ -115,9 +127,10 @@ export default class List_View_Screen extends React.Component {
       <View style={styles.container}>
         <StatusBar hidden={true} />
         <ListView
-          dataSource={this.state.dataSource}
+          dataSource={ds.cloneWithRows(this.state.events)}
           renderRow={this._renderRow.bind(this)}
         />
+
       </View>
     );
   }
