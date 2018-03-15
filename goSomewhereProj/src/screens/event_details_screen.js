77826d20aa10c29533c4444d50d872636aa2cac8
Andrey Almabekov <almabekov@gmail.com>
2018-03-15 15:30:50 -0300

    Comments displayed only for specific event
    Now on the event page only comments related to the event

--- goSomewhereProj/src/screens/event_details_screen.js
+++ goSomewhereProj/src/screens/event_details_screen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Text} from 'react-native';
-import {View, Image, ScrollView, StyleSheet, SectionList, Alert, Dimensions, Linking, Share, TouchableOpacity, TouchableHighlight} from 'react-native';
+import {View, Image, ScrollView, StyleSheet, SectionList, Alert, Dimensions, Linking, Share, TouchableOpacity, TouchableHighlight, FlatList} from 'react-native';
 // import {Button} from 'react-native';
 import { Button, Icon } from 'react-native-material-ui';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
@@ -15,7 +15,18 @@ import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 // var long = events[id]['longitude'];
 // var addr = events[id]['address'];
 const events = [{"id":1,"title":"Android Hackathon","category":"study","description":"Mittens was dancing on the tables","start_at":"2018-02-23T14:10:52.773Z","end_at":"2018-02-23T20:12:37.044Z","attendees":null,"created_at":"2018-02-10T18:12:44.050Z","updated_at":"2018-02-10T18:12:44.050Z","latitude":44.6374257,"longitude":-63.5872094,"address":"Goldberg Computer Science Building, 6050 University Ave, Halifax, NS B3H 1W5"},{"id":2,"title":"Party after winning Hackathon","category":"party","description":"Please bring your own drink","start_at":"2018-02-11T22:19:45.595Z","end_at":null,"attendees":null,"created_at":"2018-02-10T18:21:52.274Z","updated_at":"2018-02-10T18:21:52.274Z","latitude":44.6386448,"longitude":-63.5919118,"address":"H-1422B, 6230 Coburg Road, Halifax, NS, B3h4R2"}];
-const user = [{"id":1,"name":"Mittens","date":"25 Jan 2018","message":"Nobody petted me!"},{"id":2,"name":"Demon","date":"1 Dec 2017","message":"I said Chicken, not Ham"},{"id":3,"name":"BigCatLover","date":"2 Nov 2017","message":"Lots of heavy petting"}];
+
+user = [
+        {"event_id":1,"id":1,"name":"Mittens","date":"25 Jan 2018","message":"Nobody petted me!"},
+        {"event_id":1,"id":2,"name":"Demon","date":"1 Dec 2017","message":"I said Chicken, not Ham"},
+        {"event_id":1,"id":3,"name":"BigCatLover","date":"2 Nov 2017","message":"Lots of heavy petting"},
+        {"event_id":0,"id":4,"name":"Mittens","date":"25 Jan 2018","message":"Comment about Hackathon"},
+        {"event_id":0,"id":5,"name":"Demon","date":"1 Dec 2017","message":"Another hackathon comment"},
+        {"event_id":0,"id":6,"name":"BigCatLover","date":"2 Nov 2017","message":"Somebody really liked hackathon"}
+      ];
+
+
+
 const image_categories = [{"party":"party_category_image.jpg","study":"Computer-Cat.jpg"}];
 //const myIcon = (<Icon name="rocket" size={30} color="#900" />)
 const customBlue = 'rgb(72, 133, 237)';
@@ -39,7 +50,7 @@ export default class Event_Details_Screen extends React.Component {
 constructor(props) {
   super(props);
   this.state = {
-    colorLike:'black', 
+    colorLike:'black',
     colorGoing:'black',
     message:'No one was petting me',
     checkIcon:"star-outlined",
@@ -61,7 +72,7 @@ constructor(props) {
       } else {
         this.setState({
           checkIcon: "star-outlined"
-        })      
+        })
       }
 
   }
@@ -83,16 +94,16 @@ constructor(props) {
         <View style={{width: 380, height: 75, backgroundColor:customBlue}}>
         <Text style={[styles.padding, {fontWeight:'bold', color:'white', paddingBottom: 10}]}> {events[id]['title']} </Text>
         <Text style ={[styles.padding, {color:'white'}]}> Date: {events[id]['start_at']} </Text>
-        </View> 
+        </View>
 
      <View
      style={{
         flexDirection: 'row',
         justifyContent: 'space-around',
         paddingBottom: 10,
-      }}>  
+      }}>
         <TouchableOpacity>
-          <Entypo.Button name={this.state.checkIcon} backgroundColor='transparent' color ={customBlue} size = {40} 
+          <Entypo.Button name={this.state.checkIcon} backgroundColor='transparent' color ={customBlue} size = {40}
           onPress={() => {this.changeIconName()}} />
           <Text style={{textAlign:'center', color:customBlue}}>LIKE</Text>
        </TouchableOpacity>
@@ -115,56 +126,72 @@ constructor(props) {
 
      </View>
      <View style = {styles.lineStyle}></View>
-     <View style={styles.padding}> 
+     <View style={styles.padding}>
      <Text>{events[id]['description']} </Text>
       </View>
      <View style={{ padding: 10}}>
      <View style = {styles.lineStyle}></View>
 
-      <View style={{flexDirection: 'row'}}> 
+      <View style={{flexDirection: 'row'}}>
       <MaterialIcons.Button name='date-range' backgroundColor='transparent' color = {customBlue} color = {customBlue} size = {24} paddingRight={15}/>
-      <Text style ={styles.details}>Date: {events[id]['start_at']}</Text>  
+      <Text style ={styles.details}>Date: {events[id]['start_at']}</Text>
       </View>
        <View style = {styles.lineStyle}></View>
 
-      <View style={{flexDirection: 'row'}}> 
+      <View style={{flexDirection: 'row'}}>
       <MaterialCommunityIcons.Button name='tag-text-outline' backgroundColor='transparent' color = {customBlue} size = {24} paddingRight={15}/>
-      <Text style ={styles.details}>Category: {category}</Text>  
+      <Text style ={styles.details}>Category: {category}</Text>
       </View>
        <View style = {styles.lineStyle}></View>
 
        <TouchableOpacity onPress={() => {Linking.openURL("https://www.google.ca/maps/dir/44.6370632,-63.588217/ShiftKey+Labs,+University+Avenue,+Halifax,+Nova+Scotia/@44.6373505,-63.590179,17z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x4b5a223ad04ecb89:0x3e27d1ed7170b86b!2m2!1d-63.5871719!2d44.6374024!3e3?hl=ru")}}>
-      <View style={{flexDirection: 'row'}}> 
+      <View style={{flexDirection: 'row'}}>
       <MaterialIcons.Button name='location-on' backgroundColor='transparent' color = {customBlue} size = {24} paddingRight={15}/>
-      <Text style ={[styles.details, {flex:1, flexWrap:'wrap'}]}>Address: {events[id]['address']}</Text>  
+      <Text style ={[styles.details, {flex:1, flexWrap:'wrap'}]}>Address: {events[id]['address']}</Text>
       </View>
       </TouchableOpacity>
-      
+
        <View style = {styles.lineStyle}></View>
-       
+
        <TouchableOpacity onPress={() => {Linking.openURL('tel:1234567890')}}>
-       <View style={{flexDirection: 'row'}}> 
+       <View style={{flexDirection: 'row'}}>
       <MaterialIcons.Button name='phone' backgroundColor='transparent' color = {customBlue} size = {24} paddingRight={15}/>
-      <Text style ={styles.details}>Phone no: (902) CAT-CATS</Text> 
-      </View> 
+      <Text style ={styles.details}>Phone no: (902) CAT-CATS</Text>
+      </View>
       </TouchableOpacity>
 
-       <View style = {styles.lineStyle}></View>       
+       <View style = {styles.lineStyle}></View>
        </View>
 
-       <View style={[styles.padding, {flex:1}]}>
+{/*     <View style={[styles.padding, {flex:1}]}>
        <Text style={{fontSize:13, fontWeight:'bold'}}> Reviews</Text>
      <SectionList
           sections={[
           {title: <Text>{user[0]['name'] + ': ' + user[0]['date']}</Text>, data: [<Text>{user[0]['message']}</Text>]},
           {title: <Text>{user[1]['name'] + ': ' + user[1]['date']}</Text>, data: [<Text>{user[1]['message']}</Text>]},
-          {title: <Text>{user[2]['name'] + ': ' + user[2]['date']}</Text>, data: [<Text>{user[2]['message']}</Text>]},]}          
+          {title: <Text>{user[2]['name'] + ': ' + user[2]['date']}</Text>, data: [<Text>{user[2]['message']}</Text>]},]}
           renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
           renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
           keyExtractor={(item, index) => index}
         />
         </View>
-       <Button primary text="Add a Comment" onPress={() => this.props.navigation.navigate('Comments', {id:0})} />
+*/}
+
+
+
+
+
+
+       <View>
+       <Text style={{fontSize:13, fontWeight:'bold'}}>Reviews</Text>
+
+       <FlatList
+       data={user.filter(word => word['event_id']==id)}
+       renderItem={({item}) => <Text style={styles.sectionHeader}>{item['name'] + ': ' + item['date'] + '\n'+ item['message']}</Text>
+                  }
+       />
+      </View>
+      <Button primary text="Add a Comment" onPress={() => this.props.navigation.navigate('Comments', {id:0})} />
      </ScrollView>
             <View style={{
                flexDirection: 'row',
@@ -217,7 +244,7 @@ const styles = StyleSheet.create({
     height: 44,
   },
   details: {
-    paddingBottom: 10,   
+    paddingBottom: 10,
     paddingTop: 10,
     paddingRight: 10,
     fontSize: 13,
@@ -227,11 +254,9 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around'
   },
   padding: {
-    paddingBottom: 10,   
+    paddingBottom: 10,
     paddingTop: 10,
     paddingRight: 10,
     paddingLeft:15,
   }
 });
-
-    
