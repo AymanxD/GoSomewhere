864038b24ccfc9750c84218269e9ca3e0ccb87c6
Ayman Mohatarem <aymanmohatarem@gmail.com>
2018-03-30 14:31:19 -0300

    Initial filter implementation

--- goSomewhereProj/src/components/map_listview_comps/FilterModel.js
+++ goSomewhereProj/src/components/map_listview_comps/FilterModel.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import { Text, TouchableHighlight, TouchableOpacity, View, CheckBox, Slider} from 'react-native';
+import { Text, View, CheckBox, Slider, } from 'react-native';
 import Modal from "react-native-modal";
+import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
 
 import { Button } from 'react-native-material-ui';
 
@@ -11,18 +12,27 @@ export default class FilterModel extends Component {
         super(props);
 
         this.state = {
-            value: 25
+            value: this.props.distance,
+            tempValue: 25,
+            time: this.props.time,
+            tempTime: 30
         }
     }
 
-    acceptChange(value){
-        this.onChange(this.state.value);
-        this.props.onPress();
+    acceptChange(){
+        this.setState({
+            value: this.state.tempValue,
+            time: this.state.tempTime
+        }, () => {
+            this.props.onChange(this.state.tempValue, this.state.time);
+            this.props.onPress();
+        });
     }
 
     rejectChange() {
         this.setState({
-            value: 25
+            tempValue: this.state.value,
+            tempTime: this.state.time
         });
 
         this.props.onPress();
@@ -30,7 +40,11 @@ export default class FilterModel extends Component {
 
     render() {
 
-        let value = this.state.value;
+        let radio_props = [
+            {label: 'Today', value: 0 },
+            {label: 'Two Weeks', value: 14 },
+            {label: 'One Month', value: 30 }
+        ];
 
         return (
             <View>
@@ -41,22 +55,21 @@ export default class FilterModel extends Component {
                     onBackdropPress={() => this.rejectChange.bind(this)}
                     >
                     <View style={{ backgroundColor: "white", padding: 16, alignSelf: "center"}}>
-                        <Text>Time range:</Text>
-                        <View style={{flex: 0.25, flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
-                            <CheckBox/>
-                            <Text>
-                                Today
-                            </Text>
-                            <CheckBox/>
-                            <Text>
-                                Two weeks
-                            </Text>
-                            <CheckBox/>
-                            <Text>
-                                One month
-                            </Text>
+                        <Text style={{margin: 16}}>Time range:</Text>
+                        <View style={{flex: 0.15, flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
+                        <RadioForm
+                            radio_props={radio_props}
+                            initial={2}
+                            buttonSize={5}
+                            formHorizontal={true}
+                            labelHorizontal={true}
+                            buttonColor={'#2196f3'}
+                            animation={true}
+                            labelStyle={{marginRight: 8, marginLeft:-4}}
+                            onPress={(option) => {this.setState({tempTime: option})}}
+                        />
                         </View>
-                        <Text>
+                        <Text style={{margin: 16}}>
                             Distance:
                         </Text>
                         <View style={{flexDirection: "row", justifyContent: "center", margin: 16}}>
@@ -67,7 +80,7 @@ export default class FilterModel extends Component {
                                 maximumValue={50}
                                 onValueChange={(val) => {
                                     this.setState({
-                                        value: val
+                                        tempValue: val
                                     });
                                 }}
                                 style={{flex: 1}}
@@ -75,11 +88,11 @@ export default class FilterModel extends Component {
                                 maximumTrackTintColor='#b7b7b7'
                             />
                             <Text>
-                                {this.state.value}
+                                {this.state.tempValue} km
                             </Text>
                         </View>
                         <View style={{flexDirection: 'row', justifyContent:'space-around'}}>
-                            <Button primary text="Accept" onPress={this.acceptChange} style={{container:{justifyContent: "flex-end", flexDirection: 'column'}}}/>
+                            <Button primary text="Accept" onPress={this.acceptChange.bind(this)} style={{container:{justifyContent: "flex-end", flexDirection: 'column'}}}/>
                             <Button primary text="Back" onPress={this.rejectChange.bind(this)} style={{container:{justifyContent: "flex-end", flexDirection: 'column'}}}/>
                         </View>
                     </View>
