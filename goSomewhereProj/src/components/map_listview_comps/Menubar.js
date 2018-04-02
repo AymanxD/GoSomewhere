52f8391bee523ec19f159ab7165ee922143f86d3
Ayman Mohatarem <aymanmohatarem@gmail.com>
2018-04-02 16:47:09 -0300

    removed unused packages

--- goSomewhereProj/src/components/map_listview_comps/Menubar.js
+++ goSomewhereProj/src/components/map_listview_comps/Menubar.js
@@ -1,34 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View,Navigator,TextInput, KeyboardAvoidingView,TouchableOpacity,
- AsyncStorage,ScrollView,List, ListView, StatusBar
-} from 'react-native';
-import PropTypes from 'prop-types';
+
 import { BottomNavigation } from 'react-native-material-ui';
 
 
 export default class MenuBar extends React.Component {
-
-  componentDidMount() {
-      //location services
-    this.watchId = navigator.geolocation.watchPosition(
-      (position) => {
-        this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          error: null,
-        });
-      },
-      (error) => this.setState({ error: error.message }),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
-    );
-  }
-
-  componentWillMount() {
-  //this.fetchData();
-
-  //location services
-  navigator.geolocation.clearWatch(this.watchId);
-}
 
   render() {
     return (
