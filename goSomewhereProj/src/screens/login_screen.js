5559f32f5ef8f8ac9c1a77a206d11ffb64faa712
Siddhant Bhardwaj <siddhant.bhardwaj16@gmail.com>
2018-04-04 00:16:22 -0300

    removed typo

--- goSomewhereProj/src/screens/login_screen.js
+++ goSomewhereProj/src/screens/login_screen.js
@@ -160,7 +160,6 @@ export default class Login_Screen extends React.Component {
     
 
     if (type === 'success') {
-      Alert.alert('', token);
       // Get the user's name using Facebook's Graph API
       const response = await fetch(
         `https://graph.facebook.com/me?access_token=${token}&fields=email,name`);
